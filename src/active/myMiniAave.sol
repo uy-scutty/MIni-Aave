1ac96dd9f729eed881c934f269c50aff62fd682f
uy_scutty <oyedokuntominiyi1234@gmail.com>
2026-04-21 11:58:22 +0100

    added oracle logic

--- src/active/myMiniAave.sol
+++ src/active/myMiniAave.sol
@@ -7,9 +7,8 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {AggregatorV3Interface} from "src/interfaces/AggregatorV3Interfaces.sol";
 
-// need to add governace contract who can call the init
-// recomended i use my wallet do not get this sha i pass in constructor
 contract MyMiniAave is ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
 
@@ -26,9 +25,9 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         address debtToken;
         bool isInitialized;
         uint256 ltv;
-
         uint256 liquidationThreshold;
         uint256 liquidationBonus;
+        address priceFeed;
     }
     constructor(address admin) Ownable(admin) {}
 
@@ -50,6 +49,8 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
     error CannotWithdraw();
     error CannotBorrow();
     error TooLow();
+    error InvalidPriceFeed();
+    error InvalidPrice();
 
     event Deposited(address indexed user, address indexed asset, uint256 amount);
     event Withdrawn(address indexed user, address indexed asset, uint256 amount);
@@ -107,24 +108,23 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         if (amount == 0) {
             revert ZeroAmount();
         }
-
         Reserve storage reserve = reserves[asset];
 
         uint256 availableLiquidity = reserve.totalDeposited - reserve.totalBorrowed;
         if (availableLiquidity < amount) {
             revert insufficientbalance();
         }
-        uint256 maxBorrow = (userBalance[msg.sender][asset] * reserve.ltv) / 10000;
-        if (userDebt[msg.sender][asset] + amount > maxBorrow) {
+
+        uint256 maxBorrow = (getTotalCollateral(msg.sender) * reserve.ltv) / 10000;
+
+        if (getTotalDebt(msg.sender) + amount > maxBorrow) {
             revert BorrowLimitReached();
         }
-
-        userDebt[msg.sender][asset] += amount;
-        reserve.totalBorrowed += amount;
-
         if (calculateHealthFactor(msg.sender) < 1e18) {
             revert CannotBorrow();
         }
+        userDebt[msg.sender][asset] += amount;
+        reserve.totalBorrowed += amount;
 
         IERC20(asset).safeTransfer(msg.sender, amount);
         DebtToken(reserve.debtToken).mint(msg.sender, amount);
@@ -160,6 +160,33 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         emit Repaid(msg.sender, asset, payAmount);
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function getAssetPrice(address asset) public view returns (uint256) {
+        Reserve storage reserve = reserves[asset];
+        address feed = reserve.priceFeed;
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(feed);
+        (, int256 price,,,) = dataFeed.latestRoundData();
+
+        if (price <= 0) revert InvalidPrice();
+
+        uint8 decimals = dataFeed.decimals();
+        uint256 normalizedPrice = (uint256(price) * 1e18) / (10 ** decimals);
+
+        return uint256(normalizedPrice);
+    }
+
+    function getUserAssetValue(address user, address asset) public view returns (uint256) {
+        uint256 price = getAssetPrice(asset);
+        uint256 amount = userBalance[user][asset];
+
+        uint256 value = (amount * price) / 1e18;
+
+        return value;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     function getBalance(address user, address asset) public view returns (uint256) {
         _requireInitialized(asset);
 
@@ -167,22 +194,125 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         return balanceOfUser;
     }
 
-    // fix this add access control like who can call basically i feel like only this contract should be able to init a reserve or should be able to init a reserve
-    // not there is the thing about users can add they own fake token but protocol cannot add fake token
+    function getTotalCollateral(address user) public view returns (uint256) {
+        uint256 length = reserveList.length;
+        uint256 totalCollateral;
+
+        for (uint256 i = 0; i < length; i++) {
+            address asset = reserveList[i];
+            uint256 amount = userBalance[user][asset];
+
+            if (amount == 0) continue;
+
+            uint256 price = getAssetPrice(asset);
+
+            totalCollateral += (amount * price) / 1e18;
+        }
+        return totalCollateral;
+    }
+
+    function getTotalDebt(address user) public view returns (uint256) {
+        uint256 length = reserveList.length;
+        uint256 totalDebt_;
+
+        for (uint256 i = 0; i < length; i++) {
+            address asset = reserveList[i];
+            uint256 debt = userDebt[user][asset];
+
+            if (debt == 0) continue;
+
+            uint256 price = getAssetPrice(asset);
+
+            totalDebt_ += (debt * price) / 1e18;
+        }
+        return totalDebt_;
+    }
+
+    function calculateHealthFactor(address user) public view returns (uint256) {
+        uint256 totalDebt_ = getTotalDebt(user);
+        if (totalDebt_ == 0) {
+            return type(uint256).max;
+        }
+
+        uint256 length = reserveList.length;
+        uint256 accruedCollateral;
+
+        for (uint256 i = 0; i < length; i++) {
+            address asset = reserveList[i];
+            uint256 amount = userBalance[user][asset];
+
+            if (amount == 0) continue;
+
+            uint256 price = getAssetPrice(asset);
+
+            uint256 value = (amount * price) / 1e18;
+
+            uint256 threshold = reserves[asset].liquidationThreshold;
+            accruedCollateral += (value * threshold) / 10000;
+        }
+        uint256 healthFactor = accruedCollateral / totalDebt_;
+        return healthFactor;
+    }
+
+    function isLiquidatable(address user) public view returns (bool) {
+        return calculateHealthFactor(user) < 1e18;
+    }
+
+    function liquidate(address user, address debtAsset, address collateralAsset, uint256 repayAmount)
+        public
+        nonReentrant
+    {
+        if (!isLiquidatable(user)) revert NotLiquidatable();
+        if (repayAmount == 0) revert ZeroAmount();
+
+        _requireInitialized(debtAsset);
+        _requireInitialized(collateralAsset);
+
+        uint256 debtPrice = getAssetPrice(debtAsset);
+        uint256 collateralPrice = getAssetPrice(collateralAsset);
+
+        uint256 repayValue = (repayAmount * debtPrice) / 1e18;
+        uint256 bonus = reserves[collateralAsset].liquidationBonus;
+
+        uint256 adjustedValue = (repayValue * (10000 + bonus)) / 10000;
+
+        uint256 collateralToSeize = (adjustedValue * 1e18) / collateralPrice;
+
+        uint256 userCollateral = userBalance[user][collateralAsset];
+        if (collateralToSeize > userCollateral) {
+            collateralToSeize = userCollateral;
+        }
+        IERC20(debtAsset).safeTransferFrom(msg.sender, address(this), repayAmount);
+
+        userDebt[user][debtAsset] -= repayAmount;
+        reserves[collateralAsset].totalBorrowed -= repayAmount;
+
+        DebtToken(reserves[debtAsset].debtToken).burn(user, repayAmount);
+
+        userBalance[user][collateralAsset] -= collateralToSeize;
+        reserves[collateralAsset].totalDeposited -= collateralToSeize;
+
+        AToken(reserves[collateralAsset].aToken).burn(user, collateralToSeize);
+
+        IERC20(collateralAsset).safeTransfer(msg.sender, collateralToSeize);
+        emit Liquidated(user, debtAsset, collateralAsset, repayAmount);
+    }
+
     function initReserve(
         address asset,
         address _aToken,
         address _debtToken,
         uint256 _ltv,
         uint256 _liquidationThreshold,
-        uint256 _liquidationBonus
+        uint256 _liquidationBonus,
+        address _priceFeed
     ) external onlyOwner {
         Reserve storage reserve = reserves[asset];
 
         if (reserve.isInitialized) {
             revert ReserveAlreadyInitialized();
         }
-        _validateReserve(asset, _aToken, _debtToken, _ltv, _liquidationThreshold, _liquidationBonus);
+        _validateReserve(asset, _aToken, _debtToken, _ltv, _liquidationThreshold, _liquidationBonus, _priceFeed);
 
         reserves[asset] = Reserve({
             totalDeposited: 0,
@@ -192,7 +322,8 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
             ltv: _ltv,
             totalBorrowed: 0,
             liquidationThreshold: _liquidationThreshold,
-            liquidationBonus: _liquidationBonus
+            liquidationBonus: _liquidationBonus,
+            priceFeed: _priceFeed
         });
 
         reserveList.push(asset);
@@ -204,7 +335,8 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         address _debtToken,
         uint256 _ltv,
         uint256 _liquidationThreshold,
-        uint256 _liquidationBonus
+        uint256 _liquidationBonus,
+        address _priceFeed
     ) internal pure {
         if (_ltv > 10000) {
             revert InvalidLtv();
@@ -228,106 +360,14 @@ contract MyMiniAave is ReentrancyGuard, Ownable {
         if (_debtToken == address(0)) {
             revert InvalidDebtToken();
         }
+        if (_priceFeed == address(0)) {
+            revert InvalidPriceFeed();
+        }
     }
 
     function _requireInitialized(address asset) internal view {
         if (!reserves[asset].isInitialized) {
             revert ReserveNotInitialized();
         }
-    }
-
-    function getTotalCollateral(address user) public view returns (uint256) {
-        uint256 length = reserveList.length;
-        uint256 totalCollateral;
-
-        for (uint256 i = 0; i < length; i++) {
-            address asset = reserveList[i];
-            totalCollateral += userBalance[user][asset];
-        }
-        return totalCollateral;
-    }
-
-    function getTotalDebt(address user) public view returns (uint256) {
-        uint256 length = reserveList.length;
-        uint256 totalDebt_;
-
-        for (uint256 i = 0; i < length; i++) {
-            address asset = reserveList[i];
-            totalDebt_ += userDebt[user][asset];
-        }
-        return totalDebt_;
-    }
-
-    function calculateHealthFactor(address user) public view returns (uint256) {
-        uint256 totalDebt_ = getTotalDebt(user);
-        if (totalDebt_ == 0) {
-            return type(uint256).max;
-        }
-
-        uint256 length = reserveList.length;
-        uint256 accruedCollateral;
-
-        for (uint256 i = 0; i < length; i++) {
-            address asset = reserveList[i];
-            // Reserve storage reserve = reserves[asset];
-            uint256 balance = userBalance[user][asset];
-
-            if (balance == 0) continue;
-            uint256 threshold = reserves[asset].liquidationThreshold;
-            accruedCollateral += (balance * threshold) / 10000;
-        }
-        uint256 healthFactor = (accruedCollateral * 1e18) / totalDebt_;
-        return healthFactor;
-    }
-
-    function isLiquidatable(address user) public view returns (bool) {
-        return calculateHealthFactor(user) < 1e18;
-    }
-
-    function liquidate(address user, address debtAsset, address collateralAsset, uint256 repayAmount)
-        public
-        nonReentrant
-    {
-        if (!isLiquidatable(user)) revert NotLiquidatable();
-        if (repayAmount == 0) revert ZeroAmount();
-
-        _requireInitialized(debtAsset);
-        _requireInitialized(collateralAsset);
-
-        uint256 debt = userDebt[user][debtAsset];
-
-        uint256 maxRepay = debt <= 2 ? debt : debt / 2;
-
-        if (repayAmount > maxRepay) {
-            revert TooMuchPay();
-        }
-
-        uint256 actualRepay = repayAmount;
-
-        IERC20(debtAsset).safeTransferFrom(msg.sender, address(this), actualRepay);
-
-        userDebt[user][debtAsset] -= actualRepay;
-        reserves[debtAsset].totalBorrowed -= actualRepay;
-
-        DebtToken(reserves[debtAsset].debtToken).burn(user, actualRepay);
-
-        uint256 bonus = reserves[collateralAsset].liquidationBonus;
-
-        uint256 collateralToSeize = (actualRepay * (10000 + bonus)) / 10000;
-
-        uint256 userCollateral = userBalance[user][collateralAsset];
-
-        if (collateralToSeize > userCollateral) {
-            collateralToSeize = userCollateral;
-        }
-
-        userBalance[user][collateralAsset] -= collateralToSeize;
-        reserves[collateralAsset].totalDeposited -= collateralToSeize;
-
-        AToken(reserves[collateralAsset].aToken).burn(user, collateralToSeize);
-
-        IERC20(collateralAsset).safeTransfer(msg.sender, collateralToSeize);
-
-        emit Liquidated(user, debtAsset, collateralAsset, actualRepay);
     }
 }
